b0223036a7aaa07036c2478cccfceef42664f2b4
Peter Mouland <peter.mouland@gmail.com>
2020-08-28 00:45:13 +0100

    deps:bump

---
+++ .sonarrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    internal: false,
+    external: true,
+    patch: true,
+    minor: true,
+    major: false,
+    deps: true,
+    devDeps: true,
+    peerDeps: true,
+    ignoreScopes: [],
+    internalScopes: [],
+};
