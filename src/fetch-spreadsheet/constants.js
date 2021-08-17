0826943b3299dfd43c51d7c0d2493ae7fc00c19d
Peter Mouland <peter.mouland@gmail.com>
2021-08-17 20:27:31 +0100

    update sheet id's

--- src/fetch-spreadsheet/constants.js
+++ src/fetch-spreadsheet/constants.js
@@ -3,8 +3,8 @@ module.exports = {
         // TRANSFERS_ID: '10xathUydH-GDTLjngRXioaUVqBZoiZqfjfM6fhgUcYk', // transfers
         // DRAFT_ID: '1gVEHnzHPfSR7isLNfxJxq8DKKLY4mKeiffwUb7YfFlc', // teams + draft picks
         // SETUP_ID: '1HoInFwqCFLSl0yh8JBvQEFFjOg5ImiiT-BY_aDCy0AU', // game-weeks + players
-        TRANSFERS_ID: '1Bxwu_QGjO6YDLyJyByl4Yhhltbcpxcve8H2dUYZ5NNo', // transfers
-        DRAFT_ID: '1vtP55at4LszC3rM8va4oshzlnEE7c5X3tYsOiFOudbk', // teams + draft picks
-        SETUP_ID: '1v_NHXfYGSzmIer0xO2frLvRxiE3N13JXpEvgMVyHlVI', // game-weeks + players
+        SETUP_ID: '1UKOeKiApAoNMXj8id3wfaTq9HrO2zHz5_yO2TqCah7c', // game-weeks, players, teams + draft picks
+        TRANSFERS_ID: '1B5WBkm7bHmRsee3eCk7-X2e-lzqnXEWNnc7H55oULYo', // transfers
+        DRAFT_ID: '1UKOeKiApAoNMXj8id3wfaTq9HrO2zHz5_yO2TqCah7c', // teams + draft picks
     },
 };
