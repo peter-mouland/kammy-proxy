3bdc931696ede35abb6cb2fd09bf2ae5f0eaca9a
Peter Mouland <peter.mouland@gmail.com>
2022-07-31 18:33:19 +0100

    update spreadsheet id

--- src/fetch-spreadsheet/constants.js
+++ src/fetch-spreadsheet/constants.js
@@ -3,8 +3,8 @@ module.exports = {
         // TRANSFERS_ID: '10xathUydH-GDTLjngRXioaUVqBZoiZqfjfM6fhgUcYk', // transfers
         // DRAFT_ID: '1gVEHnzHPfSR7isLNfxJxq8DKKLY4mKeiffwUb7YfFlc', // teams + draft picks
         // SETUP_ID: '1HoInFwqCFLSl0yh8JBvQEFFjOg5ImiiT-BY_aDCy0AU', // game-weeks + players
-        SETUP_ID: '1UKOeKiApAoNMXj8id3wfaTq9HrO2zHz5_yO2TqCah7c', // game-weeks, players, teams + draft picks
-        TRANSFERS_ID: '1B5WBkm7bHmRsee3eCk7-X2e-lzqnXEWNnc7H55oULYo', // transfers
+        SETUP_ID: '1p1ICFDYdONIri91zCiW-ATBLXavpriECYP7fmd-r4oc', // game-weeks, players, teams + draft picks
+        TRANSFERS_ID: '14laYRwz5EEorx3DRygOrztBApg3DWac6-0gz43XUzk4', // transfers
         DRAFT_ID: '1UKOeKiApAoNMXj8id3wfaTq9HrO2zHz5_yO2TqCah7c', // teams + draft picks
     },
 };
